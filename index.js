068db181d7e595a04ea74b2b6461cc1bc7361921
Abdullah Awlia <aali.awlia@gmail.com>
2024-09-15 17:50:06 +0500

    learning and pushing js in this repo

---
+++ index.js
@@ -0,0 +1,9 @@
+
+gameNum = 17;
+for () {
+    prompt("Find the number in my mind between 0 and 40 to win $50 : ")
+}
+
+if (gameNum == numGuess) {
+    console.log("Congratulations! you found the number.")
+}
